9700770e441abbc5d45557db3dc91d95d029fda8
Salim Shrestha <salimshrestha98@gmail.com>
2022-06-07 12:54:21 +0545

    Added Top level admin menu

--- Task_4/admin-menu/admin-menu.php
+++ Task_4/admin-menu/admin-menu.php
@@ -7,9 +7,27 @@
  * Author URI: https://salim.com.np
  */
 
+function am_add_menu_test_email() {
+    add_menu_page(
+        'Test Email Page',
+        'Test Email',
+        'manage_options',
+        'am-admin-menu',
+        'am_add_menu_html',
+        'dashicons-cart',
+        4
+    );
+}
 
+add_action( 'admin_menu', 'am_add_menu_test_email' );
 
-
+function am_add_menu_html() {
+    ?>
+    <div class="wrap">
+        <p>This is admin menu.</p>
+    </div>
+    <?php
+}
 
 
 
