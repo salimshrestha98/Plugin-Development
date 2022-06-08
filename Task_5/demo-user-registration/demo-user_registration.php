b65df058843e1def11a438c5134a408c380c48d4
Salim Shrestha <salimshrestha98@gmail.com>
2022-06-08 10:35:51 +0545

    Create User Registration Form Template

---
+++ Task_5/demo-user-registration/demo-user_registration.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: Demo User Registration
+ * Description: This is task 5 plugin.
+ * Author: Salim Shrestha
+ * Author URI: https://salim.com.np
+ * Version: 0.0.1
+ */
+
+
+
+
+
+?>
---
+++ Task_5/demo-user-registration/templates/register_html.php
@@ -0,0 +1,38 @@
+<div class="wrap">
+    <div class="form-group">
+        <label for="email">Email</label>
+        <input type="text" name="email" id="" placeholder="abc@xyz.com">
+    </div>
+    <div class="form-group">
+        <label for="password">Password</label>
+        <input type="password" name="password" id="">
+    </div>
+    <div class="form-group">
+        <label for="username">Username</label>
+        <input type="text" name="username" id="">
+    </div>
+    <div class="form-group">
+        <label for="display_name">Display Name</label>
+        <input type="text" name="display_name" id="">
+    </div>
+    <div class="form-group">
+        <label for="first_name">First name</label>
+        <input type="text" name="first_name" id="">
+    </div>
+    <div class="form-group">
+        <label for="last_name">Last Name</label>
+        <input type="text" name="last_name" id="">
+    </div>
+    <div class="form-group">
+        <label for="role">Role</label>
+        <select name="role" id="">
+            <option value="1">Subscriber</option>
+            <option value="1">Editor</option>
+            <option value="1">Administrator</option>
+        </select>
+    </div>
+    <div class="form-group">
+        <button type="submit">Submit</button>
+    </div>
+
+</div>
