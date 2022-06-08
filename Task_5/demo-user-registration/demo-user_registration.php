d681ea77c35faae5af80a437049bc1b3dd8ebc78
Salim Shrestha <salimshrestha98@gmail.com>
2022-06-08 10:37:23 +0545

    Add Shortcode and callback function

--- Task_5/demo-user-registration/demo-user_registration.php
+++ Task_5/demo-user-registration/demo-user_registration.php
@@ -7,8 +7,21 @@
  * Version: 0.0.1
  */
 
+function dur_main( $atts = array(), $content = null ) {
+    // Checking if user is logged in
+    if ( !is_user_logged_in() ) {
+        if ( $_SERVER['REQUEST_METHOD'] == "POST") {
+            $redirect_url = $atts['redirect_after_registration'];
+            echo "The redirect url is ".$redirect_url;
+        } else {
+            include('templates/register_html.php');
+        }
+    }
+}
 
+// add_action( 'admin_menu', 'dur_add_menu' );
 
+add_shortcode( 'dur-form', 'dur_main' );
 
 
 ?>
