556d9b0303ba48a9f1cb58def94d11afd67c2334
Salim Shrestha <salimshrestha98@gmail.com>
2022-06-09 16:05:51 +0545

    Add - Metabox callback to add_meta_boxes hook

--- Task_8/meta-test/meta-test.php
+++ Task_8/meta-test/meta-test.php
@@ -6,4 +6,17 @@
  * Description: This is the task 8 to create a metabox.
  */
 
+add_action( 'add_meta_boxes', 'mt_register_meta_boxes' );
+
+function mt_register_meta_boxes() {
+    add_meta_box(
+        'mt_meta_box',
+        'Test Meta Box',
+        'mt_register_meta_box_callback',
+        'post',
+        'advanced',
+        'high'
+    );
+}
+
  ?>
