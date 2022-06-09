fcb8df31f6bc1aa0952bdcfa9a4d7bc0323f3edc
Salim Shrestha <salimshrestha98@gmail.com>
2022-06-09 13:07:38 +0545

    Added Comment Headers

---
+++ Task_7/movie_plugin/movie-plugin.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Movie Plugin
+ * Author: Salim Shrestha
+ * Author URI: https://salim.com.np
+ * Description: This is the task 6 plugin to create administration menus.
+ */
+
+ ?>
