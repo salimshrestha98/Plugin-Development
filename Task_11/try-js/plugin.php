9f3341700a221bb62c9d06ca9bf0c1a38ce427a1
Salim Shrestha <salimshrestha98@gmail.com>
2022-06-10 12:31:13 +0545

    Add - User Saving and JSON Response

--- Task_11/try-js/plugin.php
+++ Task_11/try-js/plugin.php
@@ -32,4 +32,37 @@ add_shortcode(
     'tj_user_registration_form_html'
 );
 
-?>
+add_action( 'wp_ajax_tj_register_user', 'tj_ajax_register_user' );
+
+function tj_ajax_register_user() {
+    $user_data = $_POST['formData'];
+    $username = sanitize_text_field( $user_data['username'] );
+    $email = sanitize_email( $user_data['email'] );
+    $password = sanitize_text_field( $user_data['password'] );
+    $return_text = array();
+
+    if ( ! username_exists( $username ) && ( email_exists( $email ) === false ) ) {
+        $user_id = wp_create_user(
+            $username,
+            $password,
+            $email
+        );
+
+        if ( $user_id ) {
+            $return_text['status'] = 'Success';
+            $return_text['message'] = 'New User Created Successfully.';            
+        } else {
+            $return_text['status'] = 'Failed';
+            $return_text['message'] = 'Some error occured.';
+        }
+    } else {
+        $return_text['status'] = 'Failed';
+        $return_text['message'] = "Username or Email already exists.";
+    }
+
+    $return_json = json_encode( $return_text );
+    echo $return_json;
+    exit;
+}
+
+?>  
