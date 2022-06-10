8831126e2d9d4ae54bbb62168e20e11f5af21a81
Salim Shrestha <salimshrestha98@gmail.com>
2022-06-10 12:24:57 +0545

    Add - Register and Enqueue the js file

--- Task_11/try-js/plugin.php
+++ Task_11/try-js/plugin.php
@@ -7,4 +7,20 @@
  * Description: This a task 11 plugin to add custom js and ajax.
  */
 
+function tj_register_scripts() {
+    wp_register_script(
+        'tj_main_js',
+        plugin_dir_url( __FILE__ ) . 'assets/js/main.js',
+        array(),
+        '1.0.0',
+        true
+    );
+}
+add_action( 'init', 'tj_register_scripts' );
+
+function tj_enqueue_scripts() {
+    wp_enqueue_script( 'tj_main_js' );
+}
+add_action( 'wp_enqueue_scripts', 'tj_enqueue_scripts' );
+
  ?>
