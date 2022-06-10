ced8436414d4edcce70dbedd5e71cce0ac7f08bf
Salim Shrestha <salimshrestha98@gmail.com>
2022-06-10 09:32:55 +0545

    Add - Plugin Header

---
+++ Task_10/custom-taxonomies/plugin.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+* Plugin Name: Custom Taxonomies
+* Author: Salim Shrestha
+* Author URI: https://salim.com.np
+* Description: This is Task 10. To create custom taxonomies called Fruits.
+*/
+
+?>
