ba322d3a64ca0747580192c520ac42fcce709312
Salim Shrestha <salimshrestha98@gmail.com>
2022-06-10 08:46:19 +0545

    Add - Plugin Header

---
+++ Task_9/movie-register-plugin/plugin.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Movie Register
+ * Author: Salim Shrestha
+ * Author URI: https://salim.com.np
+ * Description: A plugin to create and use custom post type Movie.
+ */
+
+ ?>
